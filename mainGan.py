1c3d7eaff2206e0f737ffad1d997ca8636a03f09
yeowon <dudnjsckrgo@naver.com>
2020-11-01 21:39:37 +0900

    2

--- mainGan.py
+++ mainGan.py
@@ -1,5 +1,23 @@
 from GANAttentionTextGenerator import Lang , GeneratorTransformerModel,DiscriminatorTransformerModel,PositionalEncoding \
     ,readLangs,weights_init
-filename="problemcorpus.csv"
+filename="problemcorpus"
+# import pandas as pd
+# df = pd.read_csv(filename)
+# print(df)
+# mylist = []
+# df['corpus'].apply(lambda x :mylist.append(x.lower()) )
+# print(mylist)
+# text="\n".join(mylist)
+# # print(text)
 
-readLangs()
+# with open("problemcorpus.txt","wt") as f:
+#     f.write(text)
+# with open("problemcorpus.txt","rt") as f:
+#     lines=f.readlines()
+#     for line in lines:
+#         print(line)
+
+
+input_lang, pairs=readLangs(filename)
+print("input_lang=================================\n",input_lang)
+# print("pairs======================\n",pairs)
